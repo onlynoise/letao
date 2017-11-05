6d47954e81b536b8c262bed60d315c4d9d20acfd
onlynoise <1095428753@qq.com>
2017-11-05 20:27:34 +0800

    登录退出

---
+++ public/src/utils.js
@@ -0,0 +1,29 @@
+
+define(['jquery'], function ($) {
+
+    // 检测登录
+    $.ajax({
+        // /api http://localhost:3000
+        url: '/api/employee/checkRootLogin',
+        type: 'get',
+        success: function (info) {
+            if(info.error) {
+                location.href = '/login.html';
+            }
+        }
+    })
+
+    // 退出登录
+    $('.logout').on('click', function () {
+        $.ajax({
+            url: '/api/employee/employeeLogout',
+            type: 'get',
+            success: function (info) {
+                if(info.success) {
+                    location.href = '/login.html';
+                }
+            }
+        })
+    })
+
+})
